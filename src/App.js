a0b3f4848f8dcaaa09cc942dd80b3ead01919a29
REzoa1 <56412380+REzoa1@users.noreply.github.com>
2022-06-11 21:35:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import { TodoList } from "./components/Todos";
+import { Todos } from "./components/Todos";
 import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <TodoList />
+      <Todos />
     </div>
   );
 }
